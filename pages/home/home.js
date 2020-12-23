483dabc0d9257a64f0231d7f8b31d91fb4977d73
ygt <1027275314@qq.com>
2020-12-23 17:37:16 +0800

    Initial commit

---
+++ pages/home/home.js
@@ -0,0 +1,76 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menu: [
+      {icon:"数码产品.png", name:"数码产品"},
+      {icon:"书籍教材.png", name:"书籍教材"},
+      {icon:"衣鞋帽伞.png", name:"衣鞋帽伞"},
+      {icon:"体育运动.png", name:"体育运动"},
+      {icon:"家用电器.png", name:"家用电器"},
+      {icon:"手工设计.png", name:"手工设计"},
+      {icon:"日常用品.png", name:"日常用品"},
+      {icon:"代步工具.png", name:"代步工具"}
+    ],
+   
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/my/my.js
@@ -0,0 +1,79 @@
+// pages/my/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    menu: [
+      {icon:"我的收藏.png", name:"我的收藏"},
+      {icon:"优惠卷.png", name:"优惠卷"},
+      {icon:"个人资料.png", name:"个人资料"},
+      {icon:"签到记录.png", name:"签到记录"},
+      {icon:"收货地址.png", name:"收货地址"},
+      {icon:"消息中心.png", name:"消息中心"},
+      {icon:"发布商品.png", name:"发布商品"},
+      {icon:"我的商品.png", name:"我的商品"},
+      {icon:"未认证.png", name:"未认证"},
+      {icon:"开通学校.png", name:"开通学校"},
+      {icon:"发布求购.png", name:"发布求购"},
+      {icon:"我的求购.png", name:"我的求购"}
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
